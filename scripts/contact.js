18f672047dc776f4eebee4d3044d11b23396e1f2
adamgibbons <adam.d.gibbons@gmail.com>
2014-10-05 16:47:27 -0700

    displays success message when form succeeds; moves scripts into dir

---
+++ scripts/contact.js
@@ -0,0 +1,44 @@
+function disableForm() {
+  $('#contact-form').find('input, textarea').attr('disabled', 'disabled');
+}
+
+function showErrorMessage() {
+ $('#error-message').show();
+}
+
+function hideErrorMessage() {
+  $('#error-message').hide();
+}
+
+function showSuccessMessage() {
+  $('#success-message').show();
+}
+
+function formIsValid(data) {
+  var isValid = true;
+  ['#name', '#email', '#message'].forEach(function(field) {
+    if (Boolean($(field).val()) === false) {
+      isValid = false;
+    }
+  });
+  return isValid;
+}
+
+function submitForm() {
+  hideErrorMessage();
+
+  var data = $('#contact-form').serializeArray();
+
+  if (formIsValid(data)) {
+    $.ajax({
+        url: "//forms.brace.io/adam.d.gibbons+marthagibbons@gmail.com", 
+        method: "POST",
+        data: data,
+        dataType: "json",
+        success: [showSuccessMessage, disableForm],
+        error: showErrorMessage
+    });
+  } else {
+    showErrorMessage();
+  }
+}
---
+++ scripts/google-analytics.js
@@ -0,0 +1,4 @@
+try {
+  var pageTracker = _gat._getTracker("UA-16242050-1");
+  pageTracker._trackPageview();
+} catch(err) {}
